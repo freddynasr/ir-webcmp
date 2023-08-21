d2d354f5cfd9e9eaf612a781ea0889b9e1167d1f
freddy nasr <freddy.e.nasr@gmail.com>
2023-08-21 13:07:15 +0300

    adding room type to mapping

--- src/components/ir-mapping/ir-mapping.tsx
+++ src/components/ir-mapping/ir-mapping.tsx
@@ -5,12 +5,11 @@ import { mapRoom, hostRoom } from '../../data';
   tag: 'ir-mapping',
 })
 export class IrMapping {
-
   @State() hostRoom: any = hostRoom;
   @State() mapped: any = [];
 
   // OLD
-  // {/* <div class="col-12 mb-1">
+  // {/*    <div class="col-12 mb-1">
   //             <div class="row mb-1">
   //               <div class="col-6 d-flex justify-content-between align-items-center">
   //                 Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
@@ -19,7 +18,6 @@ export class IrMapping {
   //                 <select class="form-control form-control-sm"></select>
   //               </div>
   //             </div>
-              
   //           </div>
   //           <div class="col-12 mb-1">
   //             <div class="row mb-1">
@@ -48,66 +46,69 @@ export class IrMapping {
   //             </div>
   //           </div> */}
 
-
   _onSelectMap(host, mapped) {
     // Add the mapped to the mapped array
     console.log(host, mapped);
     this.mapped = [...this.mapped, mapped];
-
   }
 
-
-
   _renderMapping(item) {
-
-    
     return (
-      <div class="col-12">
-      <div class="row ">
-        <div class="col-6 d-flex justify-content-between align-items-center">
-          <div>
-            {item.description}<ir-icon icon="ft-user"></ir-icon> {item.number_of_people}
+      <div class="col-12 mb-1">
+        <div class="row mb-1">
+          <div class="col-6 d-flex justify-content-between align-items-center">
+            Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
           </div>
-          <ir-icon icon="la la-long-arrow-right"></ir-icon>
+          <div class="col-6">
+            <select class="form-control form-control-sm"></select>
+          </div>
         </div>
-        <div class="col-6">
-          <select class="form-control form-control-sm"
-            onChange={(event: any) => this._onSelectMap(item, event.target.value)}
-          >
-            {/* Display only the values that are not in the mapped array by comparing the values / ids */}
-            {mapRoom.filter((mapped) => this.mapped.includes(mapped.id)).map((mapped) =>
-              <option
-               value={mapped.id}>{mapped.name}</option>
-            )}
-
-          </select>
+        <div class="col-12 mb-1">
+          <div class="row mb-1">
+            <div class="col-6 d-flex justify-content-between align-items-center">
+              <div>
+                {item.description}
+                <ir-icon icon="ft-user"></ir-icon> {item.number_of_people}
+              </div>
+              <ir-icon icon="la la-long-arrow-right"></ir-icon>
+            </div>
+            {}
+            <div class="col-6">
+              <select class="form-control form-control-sm" onChange={(event: any) => this._onSelectMap(item, event.target.value)}>
+                {/* Display only the values that are not in the mapped array by comparing the values / ids */}
+                {mapRoom
+                  .filter(mapped => !this.mapped.includes(mapped.id))
+                  .map(mapped => (
+                    <option value={mapped.id}>{mapped.name}</option>
+                  ))}
+              </select>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 
   render() {
     return (
-      
-        <div class="Mapping">
-          <div class="d-flex justify-content-end align-items-center">
-            <a class="text-primary">Refresh</a>
-          </div>
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-12 mb-1">
-                <div class="row ">
-                  <div class="col-6 d-flex justify-content-between align-items-center font-weight-bold">
-                    Iglooroom<ir-icon icon="la la-long-arrow-right"></ir-icon>
-                  </div>
-                  <div class="col-6 font-weight-bold">Channel Manager</div>
+      <div class="Mapping">
+        <div class="d-flex justify-content-end align-items-center">
+          <a class="text-primary">Refresh</a>
+        </div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12 mb-1">
+              <div class="row ">
+                <div class="col-6 d-flex justify-content-between align-items-center font-weight-bold">
+                  Iglooroom<ir-icon icon="la la-long-arrow-right"></ir-icon>
                 </div>
+                <div class="col-6 font-weight-bold">Channel Manager</div>
               </div>
             </div>
-              {this.hostRoom.map((item) => this._renderMapping(item))}
+            {this.hostRoom.map(item => this._renderMapping(item))}
           </div>
         </div>
+      </div>
     );
   }
 }
--- src/data.tsx
+++ src/data.tsx
@@ -1,86 +1,88 @@
 export const hostRoom = [
-    {
+  {
+    id: '1',
+    title: 'Room Type 1',
+    description: 'Room Type 1',
+    number_of_people: 2,
+  },
+  {
+    id: '2',
+    title: 'Room Type 2',
+    description: 'Room Type 2',
+    number_of_people: 4,
+  },
+  {
+    id: '3',
+    title: 'Room Type 3',
+    description: 'Room Type 3',
+    number_of_people: 5,
+    services: [
+      {
         id: '1',
-        title: 'Room Type 1',
-        description: 'Room Type 1',
-        number_of_people: 2,
-    },
-    {
-        id: '2',
-        title: 'Room Type 2',
-        description: 'Room Type 2',
-        number_of_people: 4,
-    },
-    {
-        id: '3',
-        title: 'Room Type 3',
-        description: 'Room Type 3',
-        number_of_people: 5,
-    }
-]
-
+        title: 'Service Type 1',
+      },
+    ],
+  },
+];
 
 export const mapRoom = [
-    {
-        id: '0',
-        name: '',
-        value: ''
-    },
-    {
-        id: '1',
-        name: 'Room Type 1',
-        value: 'RT1'
-    },
-    {
-        id: '2',
-        name: 'Room Type 2',
-        value: 'RT2'
-    },
-    {
-        id: '3',
-        name: 'Room Type 3',
-        value: 'RT3'
-    }
-]
+  {
+    id: '0',
+    name: '',
+    value: '',
+  },
+  {
+    id: '1',
+    name: 'Room Type 1',
+    value: 'RT1',
+  },
+  {
+    id: '2',
+    name: 'Room Type 2',
+    value: 'RT2',
+  },
+  {
+    id: '3',
+    name: 'Room Type 3',
+    value: 'RT3',
+  },
+];
 
+// OLD
+// {/* <div class="col-12 mb-1">
+//             <div class="row mb-1">
+//               <div class="col-6 d-flex justify-content-between align-items-center">
+//                 Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
+//               </div>
+//               <div class="col-6">
+//                 <select class="form-control form-control-sm"></select>
+//               </div>
+//             </div>
 
-  // OLD
-  // {/* <div class="col-12 mb-1">
-  //             <div class="row mb-1">
-  //               <div class="col-6 d-flex justify-content-between align-items-center">
-  //                 Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
-  //               </div>
-  //               <div class="col-6">
-  //                 <select class="form-control form-control-sm"></select>
-  //               </div>
-  //             </div>
-              
-  //           </div>
-  //           <div class="col-12 mb-1">
-  //             <div class="row mb-1">
-  //               <div class="col-6 d-flex justify-content-between align-items-center">
-  //                 Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
-  //               </div>
-  //               <div class="col-6">
-  //                 <div class="text-danger">Not Mapped</div>
-  //               </div>
-  //             </div>
-  //             <div class="col-12">
-  //               <div class="row ">
-  //                 <div class="col-6 d-flex justify-content-between align-items-center">
-  //                   <div>
-  //                     Service Type<ir-icon icon="ft-user"></ir-icon>2
-  //                   </div>
-  //                   <ir-icon icon="la la-long-arrow-right"></ir-icon>
-  //                 </div>
-  //                 <div class="col-6 d-flex justify-content-between">
-  //                   <div class="text-primary">
-  //                     Premium Suites <ir-icon icon="ft-user"></ir-icon>2
-  //                   </div>
-  //                   <ir-icon icon="text-primary ft-trash"></ir-icon>
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </div> */}
-
-
+//           </div>
+//           <div class="col-12 mb-1">
+//             <div class="row mb-1">
+//               <div class="col-6 d-flex justify-content-between align-items-center">
+//                 Room Type 1<ir-icon icon="la la-long-arrow-right"></ir-icon>
+//               </div>
+//               <div class="col-6">
+//                 <div class="text-danger">Not Mapped</div>
+//               </div>
+//             </div>
+//             <div class="col-12">
+//               <div class="row ">
+//                 <div class="col-6 d-flex justify-content-between align-items-center">
+//                   <div>
+//                     Service Type<ir-icon icon="ft-user"></ir-icon>2
+//                   </div>
+//                   <ir-icon icon="la la-long-arrow-right"></ir-icon>
+//                 </div>
+//                 <div class="col-6 d-flex justify-content-between">
+//                   <div class="text-primary">
+//                     Premium Suites <ir-icon icon="ft-user"></ir-icon>2
+//                   </div>
+//                   <ir-icon icon="text-primary ft-trash"></ir-icon>
+//                 </div>
+//               </div>
+//             </div>
+//           </div> */}
